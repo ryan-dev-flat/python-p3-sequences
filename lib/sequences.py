10e39b9ad89d801eb34df9b40180449f486db028
Ryan <coap.ryanmontgomery@gmail.com>
2024-06-24 23:05:29 -0600

    fibonacci?

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]
+    while len(fibonacci_sequence) < length:
+        fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+    print(fibonacci_sequence[:length])
+
